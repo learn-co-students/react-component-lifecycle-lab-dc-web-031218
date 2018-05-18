6e4ec33cc4065c67320cd330aad810c59e267d85
azeelleeza <leezaluncheon@gmail.com>
2018-05-18 15:53:21 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import Tweet from './Tweet';
+import React from "react";
+import Tweet from "./Tweet";
 
 class TweetWall extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       tweets: []
     };
   }
 
-  // TODO: componentWillMount()
+  // // TODO: componentWillMount()
+  // componentWillMount() {
+  //   this.setState({ tweets: this.props.newTweets });
+  // }
+  //
+
+  componentDidMount() {
+    this.setState({ tweets: this.props.newTweets });
+  }
+
   // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  shouldComponentUpdate = nextProps => !!nextProps.newTweets.length;
+
+  // // TODO: componentWillReceiveProps()
+  // componentWillReceiveProps(nextProps) {
+  //   this.setState({ tweets: [...nextProps.newTweets, ...this.state.tweets] });
+  // }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      tweets: [...nextProps.newTweets, ...prevState.tweets]
+    };
+  }
 
   render() {
-    const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
+    const tweets = this.state.tweets.map((tweet, index) => (
+      <Tweet text={tweet.text} key={index} />
+    ));
 
-    return (
-      <div>{tweets}</div>
-    );
+    return <div>{tweets}</div>;
   }
 }
 
